064923b28e7a952a57f43535c8c82d8daaa89f27
EdithBenvenuto <edithpau_benvenuto@hotmail.com>
2021-09-15 15:19:36 -0500

    modify contacMe

--- php/contactMeForm.php
+++ php/contactMeForm.php
@@ -1,18 +1,10 @@
-<?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $comments = $_POST['comments'];
-    
-    $email_from = 'edithpau_benvenuto@hotmail.com';
-    $email_subject = "Webpage comments";
-    $email_body = "User name: $name.\n".
-                    "User Email: $email.\n".
-                        "Comments: $comments.\n";
-    
-    $to = "edithpau_benvenuto@hotmail.com";
-    $headers = "From: $email_from\r\n";
-    $headers.= "Reply-To: $email \r\n";
-
-    mail($to,$email_subject,$email_body,$headers);
-    header("Location: index.html");
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$comment = $_POST['comment'];
+$formcontent="From: $name \n Message: $comment";
+$recipient = "edithpau_benvenuto@hotmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 ?>
